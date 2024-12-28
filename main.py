ee92e585695f41e92bb50035023bd204aa19c4e8
WesselvW <107959752+wvanweezep@users.noreply.github.com>
2024-12-28 14:09:36 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from turtle import *
+
+
+class Engine:
+  def __init__(self):
+    screen : turtle.Screen = turtle.Screen()
+    screen.tracer(0)
+    screen.bgcolor("black")
+    
+  
+
+if __name__ == "__main__":
+  instance = Engine()
